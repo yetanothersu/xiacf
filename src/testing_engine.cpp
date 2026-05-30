a886eef2c2230511678a397dadcf403c723862ce
Yasunori Watanabe <watanabe.yasunori@outlook.com>
2026-05-31 01:28:25 +0900

    fix: export C++ wrappers as internal to resolve CRAN NOTE regarding ::: usage in parallel backends

--- src/testing_engine.cpp
+++ src/testing_engine.cpp
@@ -10,7 +10,8 @@ arma::mat surrogate_iaaft_cpp(const arma::vec& x, int n_surr, int max_iter);
 void generate_single_miaaft(const arma::mat& X, arma::mat& X_surr, int max_iter);
 
 //' Compute empirical Xi-ACF and Max-Statistic null distribution
-//' @noRd
+//' @export
+//' @keywords internal
 // [[Rcpp::export]]
 List compute_xi_acf_maxstat_cpp(NumericVector x, int max_lag, int n_surr, int max_iter = 100) {
     vec y = as<vec>(x);
@@ -53,7 +54,8 @@ List compute_xi_acf_maxstat_cpp(NumericVector x, int max_lag, int n_surr, int ma
 }
 
 //' Compute empirical Xi-CCF and Max-Statistic null distribution
-//' @noRd
+//' @export
+//' @keywords internal
 // [[Rcpp::export]]
 List compute_xi_ccf_maxstat_cpp(NumericVector x, NumericVector y, int max_lag, int n_surr, int max_iter = 100, bool both_directions = true) {
     vec vx = as<vec>(x);
@@ -130,7 +132,8 @@ List compute_xi_ccf_maxstat_cpp(NumericVector x, NumericVector y, int max_lag, i
 }
 
 //' Compute empirical Xi-Matrix and Max-Statistic null distribution
-//' @noRd
+//' @export
+//' @keywords internal
 // [[Rcpp::export]]
 List compute_xi_matrix_maxstat_cpp(const arma::mat& X, int max_lag, int n_surr, int max_iter = 100) {
     int n = X.n_rows;
